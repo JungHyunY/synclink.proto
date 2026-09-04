94a6f1d20cdd77442cbb1bc637374af7a7c7c88d
JayYoon <jh.yoon@istn.co.kr>
2026-09-04 13:30:46 +0900

    chore: bump version to v1.0.5 with SyncLink Flow (KVM)

--- signaling-server/index.js
+++ signaling-server/index.js
@@ -49,7 +49,7 @@ const server = http.createServer((req, res) => {
     res.end(JSON.stringify({
       status: "ok",
       service: "synclink-signaling",
-      version: "1.0.4",
+      version: "1.0.5",
       roomsOnline: rooms.size,
       timestamp: Date.now()
     }));
